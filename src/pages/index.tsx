2b5b4cf5464fc0584c214f40c7e0534dc82d9a85
felipe alves <profelipealves@outlook.com>
2022-12-16 17:42:06 -0300

    update document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home :)</h1>
-    </div>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <div>
+        <h1>Home</h1>
+      </div>
+    </>
   );
 }
