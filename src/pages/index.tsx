ec9be144d23d718c384713193467c6ca1ba59837
felipe alves <profelipealves@outlook.com>
2022-12-19 07:42:24 -0300

    estiliza login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/home.module.scss";
+
 import logoImg from "../../public/logo.svg";
+
+import { Input } from "../components/ui/Input";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Home | ig.news</title>
+        <title>Home | Faça seu login</title>
       </Head>
-      <div>
-        <h1>Home</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo" />
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu email" type="text" />
+
+            <Input placeholder="Digite sua senha" type="password" />
+          </form>
+        </div>
       </div>
     </>
   );
