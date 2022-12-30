9774302977842db80affa7f8bcf926ecbbb2360a
felipe alves <profelipealves@outlook.com>
2022-12-30 16:31:47 -0300

    cria preview

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -2,8 +2,31 @@ import Head from "next/head";
 import styles from "./styles.module.scss";
 import { canSSRAuth } from "../../utils/canSSRAuth";
 import { Header } from "../../components/Header";
+import { FiUpload } from "react-icons/fi";
+import Image from "next/image";
+import { useState, ChangeEvent } from "react";
 
 export default function Product() {
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const [imageAvatar, setImageAvatar] = useState<File>(null);
+
+  function handleFile(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files) {
+      return;
+    }
+
+    const image = event.target.files[0];
+
+    if (!image) {
+      return;
+    }
+
+    if (image.type === "image/png" || image.type === "image/jpeg") {
+      setImageAvatar(image);
+      setAvatarUrl(URL.createObjectURL(event.target.files[0]));
+    }
+  }
+
   return (
     <>
       <Head>
@@ -14,9 +37,28 @@ export default function Product() {
         <main className={styles.container}>
           <h1>Produto</h1>
           <form className={styles.form}>
+            <label className={styles.labelAvatar}>
+              <span>
+                <FiUpload size={24} color="#fff" />
+              </span>
+              <input
+                type="file"
+                accept="image/png, image/jpeg"
+                onChange={handleFile}
+              />
+              {avatarUrl && (
+                <Image
+                  src={avatarUrl}
+                  alt="Foto do produto"
+                  width={200}
+                  height={200}
+                  className={styles.preview}
+                />
+              )}
+            </label>
             <select>
-              <option value="0">Selecione uma categoria</option>
-              <option value="0">Selecione uma categoria</option>
+              <option>Selecione uma categoria</option>
+              <option>Selecione uma categoria</option>
             </select>
             <input
               type="text"
