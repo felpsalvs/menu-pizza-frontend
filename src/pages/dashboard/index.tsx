3908d2d1b7f43ab0f0ca3a96726a26c18d4b19f0
felipe <profelipealves@outlook.com>
2023-01-03 18:41:07 -0300

    cria busca de pedidos

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,30 +1,69 @@
-import { canSSRAuth } from '../../utils/canSSRAuth';
-import { AuthContext } from '../../contexts/AuthContext';
-import Head from 'next/head';
-import { useContext } from 'react';
-import { Header } from '../../components/Header';
-import styles from './styles.module.scss';
-import { FiRefreshCcw } from 'react-icons/fi';
+import { canSSRAuth } from "../../utils/canSSRAuth";
+import { useState } from "react";
+import Head from "next/head";
+import { Header } from "../../components/Header";
+import styles from "./styles.module.scss";
+import { FiRefreshCcw } from "react-icons/fi";
+import { setupAPIClient } from "../../services/api";
 
+type OrderProps = {
+    id: string;
+    table: string | number;
+    status: boolean;
+    draft: boolean;
+    name: string | null;
+}
 
-export default function Dashboard() {
-    const { signOut } = useContext(AuthContext);
-    return (
-        <>
-            <Head>
-                <title>Dashboard | Pizzaria</title>
-            </Head>
-            <div>
-                <Header />
-            <h1>Dashboard</h1>
-            <button onClick={signOut}>Sair</button>
-            </div>
-        </>
-    )
+interface HomeProps {
+    orders: OrderProps[];
+}
+
+export default function Dashboard({ orders }: HomeProps) {
+    const [ordersList, setOrdersList] = useState(orders || []);
+
+    function handleOpenModalView(id: string) {
+        console.log(id);
+    }
+
+  return (
+    <>
+      <Head>
+        <title>Dashboard | Pizzaria</title>
+      </Head>
+      <div>
+        <Header />
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Últimos pedidos</h1>
+            <button>
+              <FiRefreshCcw size={25} color="#3afffa3" />
+            </button>
+          </div>
+
+          <article className={styles.listOrders}>
+
+            {ordersList.map( item => (
+            <section key={item.id} className={styles.orderItem}>
+              <button onClick={() => handleOpenModalView(item.id)}>
+                <div className={styles.tag}></div>
+                <span>{item.table}</span>
+              </button>
+            </section>
+            ))}
+          </article>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
-    return {
-        props: {}
-    }
-})
+    const apiClient = setupAPIClient(ctx);
+    const response = await apiClient.get("/prders");
+
+  return {
+    props: {
+        orders: response.data,
+    },
+  };
+});
